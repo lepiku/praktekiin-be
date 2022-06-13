4eb38570767f25a0eda5b92e5a4c075bc95f304f
Lepiku <git.lepiku@gmail.com>
2022-06-13 16:45:41 +0700

    create tests for /rekam_medis/pasien/

---
+++ rekam_medis/tests/test_views.py
@@ -0,0 +1,76 @@
+import json
+
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, APITestCase
+
+from rekam_medis.models import Pasien
+from rekam_medis.serializers import PasienSerializer
+
+
+class PasienTest(APITestCase):
+    url = '/rekam_medis/pasien/'
+
+    def setUp(self):
+        Pasien.objects.create(
+            nama='Dimas',
+            nama_kk='Dimas',
+            alamat='Depok',
+            pekerjaan='Mahasiswa',
+            tanggal_lahir='2000-01-01',
+            no_telp='0123456789012',
+        )
+
+    def test_pasien_list(self):
+        response = self.client.get(self.url)
+        serializer = PasienSerializer(Pasien.objects.all(), many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_pasien_create(self):
+        data = {
+            'nama': 'Tasya',
+            'nama_kk': 'Dimas',
+            'alamat': 'Depok',
+            'pekerjaan': 'Mahasiswa',
+            'tanggal_lahir': '2000-01-01',
+            'no_telp': '0123456789012',
+        }
+
+        response = self.client.post(self.url, data)
+        serializer = PasienSerializer(Pasien.objects.all(), many=True)
+        data['id'] = 2
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, data)
+        self.assertEqual(Pasien.objects.count(), 2)
+
+    def test_pasien_retrieve(self):
+        response = self.client.get(self.url + '1/')
+        serializer = PasienSerializer(Pasien.objects.get(id=1))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_pasien_update(self):
+        data = {
+            'nama': 'Dimas',
+            'nama_kk': 'Dimas',
+            'alamat': 'Depok',
+            'pekerjaan': 'PNS',
+            'tanggal_lahir': '2000-01-01',
+            'no_telp': '0123456789012',
+        }
+        response = self.client.put(self.url + '1/', data)
+        data['id'] = 1
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, data)
+
+    def test_pasien_destroy(self):
+        response = self.client.delete(self.url + '1/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Pasien.objects.count(), 0)
